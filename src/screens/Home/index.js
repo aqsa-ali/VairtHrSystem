a568fef1296a6eca8a917db634d395ade72b8055
aqsa ali <mali8424124@gmail.com>
2026-04-14 19:50:34 +0500

    react native vector icons config add

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,20 @@
 import { Alert, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { IconNames, IconTypes } from '../../services/icons'
 
 const Home = () => {
   const { loading } = useSelector(state => state.userState)
   return (
     <View style={styles.container}>
       <Text>Home</Text>
+
+      <IconTypes.MaterialIcons
+        // onPress={onRequestClose}
+        name={IconNames.close}
+        size={24}
+        color={'red'}
+      />
     </View>
   )
 }
---
+++ src/services/icons.js
@@ -0,0 +1,41 @@
+import Feather from 'react-native-vector-icons/Feather';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import AntDesign from 'react-native-vector-icons/AntDesign'
+import EvilIcons from 'react-native-vector-icons/EvilIcons'
+import Entypo from 'react-native-vector-icons/Entypo'
+
+export const IconNames = {
+    mail: 'mail',
+    check: 'check',
+    close: 'close',
+    arrow_left: 'arrow-left',
+    back: 'arrow-back',
+    arrow_drop_down: 'arrow-drop-down',
+    check_square: 'check-square',
+    calendar: 'calendar',
+    close_circle: 'close-o',
+    arrow_back: 'arrow-back-ios',
+    arrow_forward: 'arrow-forward-ios',
+    bed: 'bed',
+    bathroom: 'bathroom',
+    location: 'location-pin',
+    settings: 'settings',
+    chevron_down: 'chevron-down',
+    chevron_up: 'chevron-up',
+    un_check_radio: 'radio-button-unchecked',
+    check_radio: 'radio-button-checked',
+    visibility_off: 'visibility-off',
+    user_x: 'user-x',
+    user_check: 'user-check'
+
+}
+
+export const IconTypes = {
+    Feather,
+    SimpleLineIcons,
+    MaterialIcons,
+    AntDesign,
+    EvilIcons,
+    Entypo
+}
